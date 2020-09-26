b2d8b38b9433844aebea9ad63d6e65922abb17e2
Christopher Smith <christopher.s.smith@stonybrook.edu>
2020-09-25 22:53:16 -0400

    stuff

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // server.js
+const port = 5000;
+
+
+const express = require("express");
+const app = express();
+// const bodyparser = require("bodyparser");
+
+//app.use(bodyparser.json());         
+// app.use(bodyparser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => { 
+	console.log(req.body.value) 
+	// outputs input's value 
+}) 
+ 
+app.listen(port, () => { 
+	console.log('App running..') 
+})
