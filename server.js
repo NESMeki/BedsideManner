fd0a46c85d2a7ed59520c8191a68f87f62d0f79b
Christopher Smith <christopher.s.smith@stonybrook.edu>
2020-09-25 23:19:52 -0400

    ruff server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 // server.js
 const port = 5000;
-
-
 const express = require("express");
 const app = express();
-// const bodyparser = require("bodyparser");
+const bodyparser = require("bodyparser");
 
-//app.use(bodyparser.json());         
-// app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());         
+app.use(bodyparser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => { 
-	console.log(req.body.value) 
-	// outputs input's value 
+app.post('/', (req, res) => { 
+	let data = req.body.value;
+	console.log(data) 
+	res.json("success");
 }) 
  
 app.listen(port, () => { 
